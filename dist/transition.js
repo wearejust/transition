90ea6bd30881fa7cb035f7fb3db5ccc01ac00006
Emre Koc <emre.koc@wearejust.com>
2017-07-26 11:24:22 +0200

    Load new page and set title from metadata

--- dist/transition.js
+++ dist/transition.js
@@ -91,22 +91,8 @@ $(function () {
     parse();
 });
 
-function parse() {
-    $('a:not([href^="#"],[href^="mailto:"],[href^="tel:"])').each(function (index, item) {
-        item = $(item);
-        if (!item.data('TransitionItem')) {
-            item = new Item(item);
-            items.push(item);
-        }
-    });
-
-    trigger('ready');
-}
-
 function popState(e, item) {
     if (changing || location == window.location.href) return;
-    changing = true;
-    location = window.location.href;
 
     if (!item) {
         var i = void 0;
@@ -117,4 +103,31 @@ function popState(e, item) {
             }
         }
     }
+
+    if (item) {
+        changing = true;
+        location = window.location.href;
+
+        $.ajax({
+            url: item.url,
+            success: parse
+        });
+    }
+}
+
+function parse(data) {
+    if (data) {
+        var meta = $(data.match(/<head[^>]*>[\s\S]*<\/head>/i)[0]);
+        document.title = meta.filter('title').text();
+    }
+
+    $('a:not([href^="#"],[href^="mailto:"],[href^="tel:"])').each(function (index, item) {
+        item = $(item);
+        if (!item.data('TransitionItem')) {
+            item = new Item(item);
+            items.push(item);
+        }
+    });
+
+    trigger('ready');
 }
--- src/transition.js
+++ src/transition.js
@@ -13,22 +13,8 @@ $(function() {
     parse();
 });
 
-function parse() {
-    $('a:not([href^="#"],[href^="mailto:"],[href^="tel:"])').each(function(index, item) {
-        item = $(item);
-        if (!item.data('TransitionItem')) {
-            item = new Item(item);
-            items.push(item);
-        }
-    });
-
-    trigger('ready');
-}
-
 function popState(e, item) {
     if (changing || location == window.location.href) return;
-    changing = true;
-    location = window.location.href;
     
     if (!item) {
         let i;
@@ -39,6 +25,31 @@ function popState(e, item) {
             }
         }
     }
-
     
+    if (item) {
+        changing = true;
+        location = window.location.href;
+
+        $.ajax({
+            url: item.url,
+            success: parse
+        });
+    }
+}
+
+function parse(data) {
+    if (data) {
+        let meta = $(data.match(/<head[^>]*>[\s\S]*<\/head>/i)[0]);
+        document.title = meta.filter('title').text();
+    }
+    
+    $('a:not([href^="#"],[href^="mailto:"],[href^="tel:"])').each(function(index, item) {
+        item = $(item);
+        if (!item.data('TransitionItem')) {
+            item = new Item(item);
+            items.push(item);
+        }
+    });
+
+    trigger('ready');
 }
